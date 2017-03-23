d1fd5d61ec827f7fce0f6a76a659193483232460
Thomas Gfüllner <thomas.gfuellner@siemens.com>
2017-03-23 13:28:23 +0100

    Parsing of http response hostname

--- src/test/java/example/HttpFailoverExample.java
+++ src/test/java/example/HttpFailoverExample.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HttpFailoverExample {
 
@@ -22,22 +24,27 @@ public class HttpFailoverExample {
         cm.setMaxTotal(100);
         FailoverHttpClient failoverHttpClient = new FailoverHttpClient(cm);
 
+        final String dockerIp = "192.168.159.139";
 
         // have ready a list of hosts to try the call
         List<HttpHost> hosts = Arrays.asList(
-            new HttpHost("localhost", 9090),
-            new HttpHost("localhost", 9191)
+            new HttpHost(dockerIp, 9090),
+            new HttpHost(dockerIp, 9091)
         );
 
         // create the request
         HttpGet request = new HttpGet(URI.create("/file.txt"));
 
-        // invoke the request on localhost:9090 first,
-        // and localhost:9191 if that fails.
+        // invoke the request on dockerIp:9090 first and dockerIp:9091 if that fails.
 
         try {
             HttpResponse response = failoverHttpClient.execute(hosts, request);
-            System.out.println("One of the hosts responded with " + EntityUtils.toString(response.getEntity()));
+            String res = EntityUtils.toString(response.getEntity());
+            Pattern pattern = Pattern.compile(".*hostname is (\\w*).*", Pattern.DOTALL);
+            Matcher m = pattern.matcher(res);
+            if (m.matches()) {
+            	System.out.println("Responding host was: "  + m.group(1));
+            }
         }
         catch(IOException ex) {
             System.err.println("both hosts failed. The last exception is " + ex);
